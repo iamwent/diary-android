b5aa176b603d3456502a48eb68b3eca6059f1593
iamwent <ilovewent@gmail.com>
2017-03-05 23:15:42 +0800

    config fabric

--- app/src/main/java/com/iamwent/diary/DiaryApplication.java
+++ app/src/main/java/com/iamwent/diary/DiaryApplication.java
@@ -2,6 +2,9 @@ package com.iamwent.diary;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 /**
@@ -14,6 +17,8 @@ public class DiaryApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Fabric.with(this, new Crashlytics());
 
         CalligraphyConfig config = new CalligraphyConfig.Builder()
                 .setDefaultFontPath("fonts/FZLongZhaoFW.ttf")
